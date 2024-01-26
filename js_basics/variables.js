83c8b38d5786235ba216966ce933468787c5d36b
geeky-brahma <sahoobrahmananda2004.com>
2024-01-26 23:57:13 +0530

    created and edited variables.js

---
+++ js_basics/variables.js
@@ -0,0 +1,19 @@
+const accountId = 12345
+let accountName = "Brahma"
+var accountType = "Free"
+accountCity = "Bhubaneswar"
+let accountState // If no value assigned then it is initialised as undefined 
+
+// accountId = 2  // Variables declared with const keyword cant be changed once a value has been assigned to it.
+// console.log(accountId);
+
+accountName = "Nanda"
+accountType = "paid"
+accountCity = "Puri"
+
+console.table([accountId, accountName, accountType, accountCity, accountState])
+
+
+/**
+    Prefer not to use var keyword as it has some issues with the block scope and functional scope
+*/  
