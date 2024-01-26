2ab88ab142c5c33bcf88e9a22dd0ff8bd4642185
geeky-brahma <sahoobrahmananda2004.com>
2024-01-27 02:22:29 +0530

    Stack and Heap memory edited

--- js_basics/datatype_summary.js
+++ js_basics/datatype_summary.js
@@ -15,6 +15,8 @@
        Object  =>  object
  */
 
+const { name } = require("tar/lib/types");
+
 // Arrays
 let myArr = [1,2,3,4,5]
 console.log(myArr);
@@ -31,3 +33,38 @@ let myFun = function fun(){
     console.log("Hello World")
 }
 myFun()
+
+
+/*--------------------------------------------------------------------------------------*/
+
+// Stack and Heap Memory
+
+// Stack -> All Primitive Data types. Call by Value. 
+// Heap -> Non Primitive Data types. Call by Reference
+
+// Stack Operation: When the new variable is changed its not reflected in the original variable
+
+let nameOne = "Brahma"
+let nameTwo = nameOne
+
+nameTwo = "Sahoo"
+
+console.log(nameOne)
+console.log(nameTwo)
+
+
+// Heap Operation: Here when the something in the new variable is changed then its reflected in the original variable
+
+let myObj1 = {
+    name:"Brahma",
+    pwd:"Password"
+}
+
+let myObj2 = myObj1
+myObj2.pwd = "pass"
+
+console.log(myObj1);
+console.log(myObj2);
+
+
+
