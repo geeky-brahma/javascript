e4133bd323666af6dcd9040993b96e9db49cd954
Brahmananda Sahoo <78798925+geeky-brahma@users.noreply.github.com>
2024-03-25 13:26:08 +0530

    loops

--- iterators/loops.js
+++ iterators/loops.js
@@ -16,7 +16,7 @@ const map = new Map()
 map.set("1","a")
 map.set("2","b")
 map.set("3","c")
-console.log(map);
+// console.log(map);
 
 for (const [key, value] of map) {
     // console.log(`${key} :- ${value}`);
@@ -46,3 +46,42 @@ for (const key in arr) {
 }
 
 // But we cant use for...in loop in maps as they are not iterable
+
+
+const coding = ["js", "python", "cpp"];
+coding.forEach( function (item) {
+    // console.log(item);   // js, python, cpp 
+} )
+
+coding.forEach( (item) => {
+    // console.log(item);   // // js, python, cpp 
+} )
+
+function printMe(item) {
+    console.log(item);   
+}
+// coding.forEach(printMe);    // js, python, cpp 
+
+coding.forEach( (item, index, arr) => {
+    // console.log(item, index, arr);
+} )
+
+
+// For each is useful in arrays containing objects as its elements: 
+const myCoding =[
+    {
+        language : "Javascript",
+        fileName : "js"
+    },
+    {
+        language : "Java",
+        fileName : "java"
+    },
+    {
+        language : "Python",
+        fileName : "py"
+    }
+]
+myCoding.forEach( (item) => {
+    console.log(item.language);
+} );
